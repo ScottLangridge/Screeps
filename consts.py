1c71568d7cf5d1701137df98acb519340eedc7f0
Scott Langridge <scott.m.langridge@gmail.com>
2019-03-13 11:25:55 +0000

    added new container

--- consts.py
+++ consts.py
@@ -8,7 +8,7 @@ STARTER_BODY = BODIES['starter'][2]
 HARVESTER_BODY = BODIES['harvester'][2]
 BUILDER_BODY = BODIES['builder'][2]
 
-HARVESTER_CONTAINER_FILL_ORDER = ['5c8837ab0feddb5f7827ba2c', '5c8838acfae08c153a72296d', '5c884397ea31a939846fe6b5',
-                                  '5c884a25311fe41e87188549']
+HARVESTER_CONTAINER_FILL_ORDER = ['5c8837ab0feddb5f7827ba2c', '5c88e870946eb05f77944ad6', '5c8838acfae08c153a72296d',
+                                  '5c884397ea31a939846fe6b5', '5c884a25311fe41e87188549']
 
 DEBUG_MESSAGES = False
