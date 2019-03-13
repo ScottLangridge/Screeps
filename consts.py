073c97d95b99ca8e681e57ba913b0a317bcac78b
Scott Langridge <scott.m.langridge@gmail.com>
2019-03-13 14:58:36 +0000

    stopped errors showing on harvester when there is nowhere to dump

--- consts.py
+++ consts.py
@@ -12,7 +12,7 @@ BUILDER_BODY = BODIES['builder'][2]
 HAULER_BODY = BODIES['hauler'][0]
 UPGRADER_BODY = BODIES['upgrader'][1]
 
-HARVESTER_CONTAINER_FILL_ORDER = ['5c8837ab0feddb5f7827ba2c', '5c88e870946eb05f77944ad6', '5c8838acfae08c153a72296d',
-                                  '5c884397ea31a939846fe6b5', '5c884a25311fe41e87188549']
+HARVESTER_CONTAINER_FILL_ORDER = ['5c8837ab0feddb5f7827ba2c', '5c88e870946eb05f77944ad6',
+                                  '5c884a25311fe41e87188549']
 
 DEBUG_MESSAGES = False
