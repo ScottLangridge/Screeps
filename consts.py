9c96146f2b8811386846a9d8c97bffdbc874bc01
Scott Langridge <scott.m.langridge@gmail.com>
2019-03-13 11:24:43 +0000

    formatting

--- consts.py
+++ consts.py
@@ -9,6 +9,6 @@ HARVESTER_BODY = BODIES['harvester'][2]
 BUILDER_BODY = BODIES['builder'][2]
 
 HARVESTER_CONTAINER_FILL_ORDER = ['5c8837ab0feddb5f7827ba2c', '5c8838acfae08c153a72296d', '5c884397ea31a939846fe6b5',
-                        '5c884a25311fe41e87188549']
+                                  '5c884a25311fe41e87188549']
 
 DEBUG_MESSAGES = False
