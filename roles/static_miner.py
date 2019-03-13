e683ff9d9aed86372dd96814bbd494c99d661b05
Scott Langridge <scott.m.langridge@gmail.com>
2019-03-13 15:12:28 +0000

    outline

---
+++ roles/static_miner.py
@@ -0,0 +1,76 @@
+from defs import *
+from consts import HARVESTER_CONTAINER_FILL_ORDER
+
+__pragma__('noalias', 'name')
+__pragma__('noalias', 'undefined')
+__pragma__('noalias', 'Infinity')
+__pragma__('noalias', 'keys')
+__pragma__('noalias', 'get')
+__pragma__('noalias', 'set')
+__pragma__('noalias', 'type')
+__pragma__('noalias', 'update')
+
+
+def run(me):
+    decide_task(me)
+
+    if me.memory.depositing:
+        deposit(me)
+    else:
+        collect(me)
+
+
+def decide_task(me):
+    if me.carry.energy == 0:
+        if me.memory.depositing:
+            me.say('Mining')
+        me.memory.depositing = False
+    elif me.carry.energy == me.carryCapacity:
+        if not me.memory.depositing:
+            me.say('Dropping')
+        me.memory.depositing = True
+
+
+def deposit(me):
+    target = get_target(me)
+    if target is not None:
+        code = me.transfer(target, RESOURCE_ENERGY)
+        if code == OK:
+            me.memory.target = False
+        elif code == ERR_NOT_IN_RANGE:
+            me.moveTo(target)
+        else:
+            me.say('ERR1: ' + code)
+
+
+def collect(me):
+    target = me.pos.findClosestByPath(FIND_SOURCES_ACTIVE)
+    if target is not None:
+        code = me.harvest(target)
+        if code == OK:
+            pass
+        elif code == ERR_NOT_IN_RANGE:
+            me.moveTo(target)
+        else:
+            me.say('ERR2: ' + code)
+
+
+def get_target(me):
+    spawn = Game.spawns['Spawn1']
+    filter_non_full_extensions = {'filter': lambda s: s.structureType == STRUCTURE_EXTENSION
+                                                      and s.energy < s.energyCapacity}
+    extension = me.pos.findClosestByPath(FIND_STRUCTURES, filter_non_full_extensions)
+
+    # Containers (in order of importance)
+    for container in HARVESTER_CONTAINER_FILL_ORDER:
+        cont = Game.getObjectById(container)
+        if cont.store[RESOURCE_ENERGY] < cont.storeCapacity:
+            return cont
+
+    # Spawn
+    if spawn.energy < spawn.energyCapacity:
+        return spawn
+
+    # Extensions
+    if extension is not None:
+        return extension
