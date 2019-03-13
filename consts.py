e8f30f7a09d3142b75624dc4671b76e786a8429e
Scott Langridge <scott.m.langridge@gmail.com>
2019-03-13 10:16:43 +0000

    tidying

--- consts.py
+++ consts.py
@@ -8,6 +8,6 @@ HARVESTER_BODY = BODIES['harvester'][2]
 
 # HARVESTER
 HARVESTER_CONTAINER_FILL_ORDER = ['5c8837ab0feddb5f7827ba2c', '5c8838acfae08c153a72296d', '5c884397ea31a939846fe6b5',
-                        '5c884a25311fe41e87188549']
+                                  '5c884a25311fe41e87188549']
 
 DEBUG_MESSAGES = False
