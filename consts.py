a0c1229de9e412763997309050af4c1c18d1afc3
Scott Langridge <scott.m.langridge@gmail.com>
2019-03-13 10:51:24 +0000

    Builders should now be implemented

--- consts.py
+++ consts.py
@@ -5,6 +5,7 @@ TARGET_HARVESTERS = 1
 
 STARTER_BODY = BODIES['starter'][2]
 HARVESTER_BODY = BODIES['harvester'][2]
+BUILDER_BODY = BODIES['builder'][2]
 
 CONTAINER_FILL_ORDER = ['5c8837ab0feddb5f7827ba2c', '5c8838acfae08c153a72296d', '5c884397ea31a939846fe6b5',
                         '5c884a25311fe41e87188549']
