d031a638ddea6bfc569a685929fc4587893c227e
Scott Langridge <scott.m.langridge@gmail.com>
2019-03-14 22:57:50 +0000

    Logic written for haulers (I think)

--- roles/hauler.py
+++ roles/hauler.py
@@ -1,4 +1,5 @@
 from defs import *
+from consts import HARVESTER_CONTAINER_FILL_ORDER
 
 __pragma__('noalias', 'name')
 __pragma__('noalias', 'undefined')
@@ -33,40 +34,54 @@ def decide_task(me):
 
 
 def deposit(me):
-    # Fill extensions
-    filter_extension = {'filter': lambda s: s.structureType == STRUCTURE_EXTENSION and s.energy < s.energyCapacity}
-    target = me.pos.findClosestByRange(FIND_STRUCTURES, filter_extension)
-    if target is not None:
-        code = me.transfer(target, RESOURCE_ENERGY)
-        if code == OK:
-            pass
-        elif code == ERR_NOT_IN_RANGE:
-            me.moveTo(target)
-        return
-
-    # Fill spawns
-    for spawn_name in Object.keys(Game.spawns):
-        spawn = Game.spawns[spawn_name]
-        if spawn.energy < spawn.energyCapacity:
-            target = spawn
+    if Memory.hauler_has_important_task:
+        # Fill extensions
+        filter_extension = {'filter': lambda s: s.structureType == STRUCTURE_EXTENSION and s.energy < s.energyCapacity}
+        target = me.pos.findClosestByRange(FIND_STRUCTURES, filter_extension)
+        if target is not None:
             code = me.transfer(target, RESOURCE_ENERGY)
             if code == OK:
                 pass
             elif code == ERR_NOT_IN_RANGE:
-                me.moveTo(spawn)
+                me.moveTo(target)
             return
 
-    # Fill towers
-    filter_tower = {'filter': lambda s: s.structureType == STRUCTURE_TOWER
-                    and s.energyCapacity - s.energy > me.carry[RESOURCE_ENERGY]}
-    target = me.pos.findClosestByRange(FIND_STRUCTURES, filter_tower)
-    if target is not None:
-        code = me.transfer(target, RESOURCE_ENERGY)
-        if code == OK:
-            pass
-        elif code == ERR_NOT_IN_RANGE:
-            me.moveTo(target)
-        return
+        # Fill spawns
+        for spawn_name in Object.keys(Game.spawns):
+            spawn = Game.spawns[spawn_name]
+            if spawn.energy < spawn.energyCapacity:
+                target = spawn
+                code = me.transfer(target, RESOURCE_ENERGY)
+                if code == OK:
+                    pass
+                elif code == ERR_NOT_IN_RANGE:
+                    me.moveTo(spawn)
+                return
+
+        # Fill towers
+        filter_tower = {'filter': lambda s: s.structureType == STRUCTURE_TOWER
+                        and s.energyCapacity - s.energy > me.carry[RESOURCE_ENERGY]}
+        target = me.pos.findClosestByRange(FIND_STRUCTURES, filter_tower)
+        if target is not None:
+            code = me.transfer(target, RESOURCE_ENERGY)
+            if code == OK:
+                pass
+            elif code == ERR_NOT_IN_RANGE:
+                me.moveTo(target)
+            return
+    else:
+        # Fill containers (in order of importance)
+        for container in HARVESTER_CONTAINER_FILL_ORDER:
+            target = Game.getObjectById(container)
+            if target.store[RESOURCE_ENERGY] < target.storeCapacity:
+                code = me.transfer(target, RESOURCE_ENERGY)
+                if code == OK:
+                    me.memory.target = False
+                elif code == ERR_NOT_IN_RANGE:
+                    me.moveTo(target)
+                else:
+                    me.say('ERR1: ' + code)
+                return
 
 
 def collect(me):
@@ -83,19 +98,20 @@ def collect(me):
             me.say('ERR1:' + str(code))
         return
 
-    # Take from container
-    filter_containers = {'filter': lambda s: s.structureType == STRUCTURE_CONTAINER
-                                             and s.store[RESOURCE_ENERGY] >= me.carryCapacity}
-    target = me.pos.findClosestByRange(FIND_STRUCTURES, filter_containers)
-    if target is not None:
-        code = me.withdraw(target, RESOURCE_ENERGY)
-        if code == OK:
+    if Memory.hauler_has_important_task:
+        # Take from container
+        filter_containers = {'filter': lambda s: s.structureType == STRUCTURE_CONTAINER
+                             and s.store[RESOURCE_ENERGY] >= me.carryCapacity}
+        target = me.pos.findClosestByRange(FIND_STRUCTURES, filter_containers)
+        if target is not None:
+            code = me.withdraw(target, RESOURCE_ENERGY)
+            if code == OK:
+                return
+            elif code == ERR_NOT_IN_RANGE:
+                me.moveTo(target)
+            else:
+                me.say('ERR2:' + str(code))
             return
-        elif code == ERR_NOT_IN_RANGE:
-            me.moveTo(target)
-        else:
-            me.say('ERR2:' + str(code))
-        return
 
     # Take from storage
     filter_storage = {'filter': lambda s: s.structureType == STRUCTURE_STORAGE
